6ffc892751c157c6695f991e194e51bfbd72af5e
Racheal-spec <folorunshotosin334@gmail.com>
2022-01-10 16:15:19 +0100

    changed projects

--- src/components/Skills/Frontend/ProjectsData.js
+++ src/components/Skills/Frontend/ProjectsData.js
@@ -1,16 +1,17 @@
-import project3 from "../../../Assets/IMS trending.png";
-import project1 from "../../../Assets/FF-watches.png";
+import project3 from "../../../Assets/iscut.jpg";
+import project1 from "../../../Assets/ffcut.jpg";
 import project2 from "../../../Assets/spotify-clone-app4.jpg";
 import project5 from "../../../Assets/Paybox-screenshot.png";
+import project4 from "../../../Assets/dashboard-screenshot2.jpg";
 
 export const ProjectsData = [
   {
     id: "1",
     title: "FashionFiesta",
     img: project1,
-    info: "FashionFiesta is an ecommerce website I handed coded from scratch using react js.A user can create an account, login, add items to cart, remove items from cart, view details of each item and proceed to make a payment using Paystack.",
+    info: "FashionFiesta is an ecommerce website I developed using react js.A user can create an account, login, add items to cart, remove items from cart, view details of each item and proceed to make a payment using Paystack.",
     stacks: [
-      "Tech Stack",
+      "Tech Stack:",
       "React Js",
       "SCSS",
       "Context Api",
@@ -26,7 +27,7 @@ export const ProjectsData = [
     img: project2,
     info: "This is a front-end clone of the spotify web player. The UI was modelled after the spotify desktop app using React Js, Spotify Web Api, Redux for state management and Material UI for styling.",
     stacks: [
-      "Tech Stack",
+      "Tech Stack:",
       "React Js",
       "CSS",
       "Redux",
@@ -37,29 +38,30 @@ export const ProjectsData = [
     github: "https://github.com/Racheal-spec/Tomi-spotify-clone",
   },
   {
+    id: "4",
+    title: "Paybox Dashboard",
+    img: project4,
+    info: "Paybox dashboard UI design- built with React Js and SCSS",
+    stacks: ["Tech Stack:", "React Js", "SCSS", "Material UI", "Recharts"],
+    url: "https://paybox-dashboard.netlify.app/",
+    github: "https://github.com/Racheal-spec/PayBox-Dashboard",
+  },
+  {
     id: "3",
     title: "Instamoviesearch",
     img: project3,
     info: "A react movie app built using The Movie DB Api. Users can get list of trending, popular and top rated movies. What's more? Users can also search for their favourite movies and add these movies to their watchlist.",
-    stacks: ["Tech Stack", "React Js", "SCSS", "Redux"],
+    stacks: ["Tech Stack:", "React Js", "SCSS", "Redux"],
     url: "https://instamoviesearch.netlify.app/",
     github: "https://github.com/Racheal-spec/Instasearchmovie",
   },
-  {
-    id: "4",
-    title: "Paybox Dashboard",
-    img: project5,
-    info: "Paybox dashboard UI design- built with React Js and SCSS",
-    stacks: ["Tech Stack", "React Js", "SCSS", "Material UI", "Recharts"],
-    url: "https://paybox-dashboard.netlify.app/",
-    github: "https://github.com/Racheal-spec/PayBox-Dashboard",
-  },
+
   {
     id: "5",
     title: "Paybox",
     img: project5,
     info: "A financial application landing page.P.S-landing page and all assets designed by me",
-    stacks: ["Tech Stack", "HTML", "CSS", "Javascript", "GSAP"],
+    stacks: ["Tech Stack:", "HTML", "CSS", "Javascript", "GSAP"],
     url: "https://payboxapp.netlify.app",
     github: "https://github.com/Racheal-spec/PayBox",
   },
